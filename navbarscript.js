c9d44ab6f8bd18fdf56eaecab9931c1c4d938405
Amuntiedt <amuntiedt@hotmail.com>
2026-02-01 16:36:53 -0600

    more work on webiste responsiveness, added a .js file for the script for home page, tried to adjust responsiveness in contacts, changed how the resume skill section looks at smaller sizes, tried one attempt at working with the flip cards on project page

---
+++ navbarscript.js
@@ -0,0 +1,23 @@
+
+    function showSidebar(){
+      const sidebar = document.querySelector('.sidebar')
+      sidebar.style.display = 'flex'
+    }
+    function hideSidebar(){
+      const sidebar = document.querySelector('.sidebar')
+      sidebar.style.display = 'none'
+    }
+  //~ const small-screen-navbar = document.querySelector('.small-screen-navbar');
+  //~ const navbar = document.querySelector('.navbar');
+  //~ let menuOpen = false;
+  
+  //~ small-screen-navbar.addEventListener('click', () => {
+	  //~ if (menuOpen == false) {
+		  //~ navbar.style.display = "block";
+		  //~ menuOpen = true;
+	  //~ }
+	//~ else if (menuOpen == true) {
+		//~ navbar.style.display = 'none';
+		//~ menuOpen = false;
+		//~ )
+	//~ });
